965f58c1eadd6d8ca91f63c4b63a7c414adb590e
kunal <developerkunal01@gmail.com>
2025-06-25 20:23:42 +0530

    fixed background error this time at layout

--- app/layout.js
+++ app/layout.js
@@ -5,12 +5,11 @@ import Navbar from "./Components/Navbar.jsx";
 import StarField from "./Components/StarField";
 import Footer from "./Components/Footer";
 
-// For App Router (app/layout.tsx)
 import { Tilt_Neon } from "next/font/google";
 
 const tiltNeon = Tilt_Neon({
   subsets: ["latin"],
-  weight: "400", // or 'regular'
+  weight: "400",
   variable: "--font-tilt-neon",
 });
 
@@ -24,14 +23,14 @@ export default function Layout({ children }) {
     <html lang="en" className={tiltNeon.variable}>
       <body className="bg-gray-900">
         <StarField />
-        <ClientLayout>
-          <div className="sticky top-0 z-50">
-            <Navbar />
-          </div>
-          {children}
-        </ClientLayout>
-        <div className="h-[1px] bg-amber-50"></div>
-        <div>
+        <div className="relative z-10">
+          <ClientLayout>
+            <div className="sticky top-0 z-50">
+              <Navbar />
+            </div>
+            {children}
+          </ClientLayout>
+          <div className="h-[1px] bg-amber-50"></div>
           <Footer />
         </div>
       </body>
